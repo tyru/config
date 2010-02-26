e3fc0bffa0597b7592b8b352c68f90b2c4029214
tyru <tyru.exe@gmail.com>
2010-02-26 18:31:41 +0900

    vimperator - Add, update plugins.

--- .vimperator/plugin/opener.js
+++ .vimperator/plugin/opener.js
@@ -1,54 +1,83 @@
-// via http://vimperator.g.hatena.ne.jp/nokturnalmortum/20100120/1263927707
+// http://github.com/voidy21/dotfiles/raw/master/.vimperator/plugin/opener.js
 
 
-(function () {
+liberator.registerObserver(
+  "enter",
+  function () {
     let U = liberator.plugins.libly.$U;
+    //BarTabs用
+    let mTabs = document.getElementById("content").mTabs;
 
     function jump (url) {
-        let index = 0;
-        let url = util.stringToURLArray(url).toString();
-        for each ( [,tab] in tabs.browsers ) {
-            if(url == tab.currentURI.spec){
-                tabs.select(index);
-                return true;
-            }
-            ++index;
+      let index = 0;
+      let url = util.stringToURLArray(url).toString();
+      for each ( [,tab] in tabs.browsers ) {
+        if(url == tab.currentURI.spec ||
+           url == mTabs[index].linkedBrowser.userTypedValue){
+          tabs.select(index);
+          return true;
         }
-        return false;
+        ++index;
+      }
+      return false;
     }
 
     "open tabopen edit".split(/\s/).forEach(
-        function (name) {
+      function (name) {
         let command = commands.get(name);
         if (!command)
-            return;
+          return;
         U.around(
-            command,
-            "action",
-            function (next, args) {
-                let url = args[0].string;
-                if (!(url && jump(url)))
-                    return next();
-            },
-            true
+          command,
+          "action",
+          function (next, args) {
+            let url = args[0].string;
+            if (!(url && jump(url)))
+              return next();
+          }
         );
-    }
+      }
     );
 
     //buffer.followLink()を変更
     //hint-a-hint時[f,F]に対象のタブが既に開いてあったらjump
-    U.around(
+    let (ignore = false) {
+      let ignoreBlock = function (block) {
+        ignore = true;
+        let result = block();
+        ignore = false;
+        return result;
+      };
+
+      U.around(
         buffer,
         "followLink",
         function (next, args) {
+          return ignoreBlock(function () {
             let [elem,] = args;
             let url = elem.href;
             if (!(url && jump(url))){
-                liberator.echo("Now Loading...  " + url);
-                return next();
+              liberator.echo("Now Loading... " + url);
+              return next();
             }
+          });
+        }
+      );
+
+      document.addEventListener(
+        'click',
+        function (event) {
+          if (ignore)
+            return;
+          let e = event.target;
+          if (e && e.tagName.match(/^a$/i) && e.href && jump(e.href)) {
+            event.preventDefault();
+            event.stopPropagation();
+          }
         },
         true
-    );
-})();
+      );
+    }
 
+  }
+);
