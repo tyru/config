ba8408b7cd7cb6e7c5bc60d1904fcf87f7b94ca0
tyru <tyru.exe@gmail.com>
2010-08-14 00:54:12 +0900

    vim - Fix .vim/template/*

--- .vim/template/python.py
+++ .vim/template/python.py
@@ -2,24 +2,8 @@
 # coding: utf-8
 
 
-
-def do_fizzbuzz(i):
-	if i % 15 == 0:
-		return "FizzBuzz"
-	elif i % 5 == 0:
-		return "Buzz"
-	elif i % 3 == 0:
-		return "Fizz"
-	else:
-		return str(i)
-
-def fizzbuzz(*args):
-	for i in apply(range, args):
-		yield do_fizzbuzz(i)
-
 def main():
-	for s in fizzbuzz(1, 100):
-		print s
+    pass
 
 if __name__ == '__main__':
-	main()
+    main()
