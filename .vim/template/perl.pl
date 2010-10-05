bff269cc2286617886d958437783a5776c95cf90
tyru <tyru.exe@gmail.com>
2010-10-06 06:41:44 +0900

    vim - Fix perl template file

--- .vim/template/perl.pl
+++ .vim/template/perl.pl
@@ -3,7 +3,10 @@ use strict;
 use warnings;
 use utf8;
 
-use Getopt::Long;
+# gnu_compat: --opt="..." is allowed.
+# no_bundling: single character option is not bundled.
+# no_ignore_case: no ignore case on long option.
+use Getopt::Long qw(:config gnu_compat no_bundling no_ignore_case);
 use Pod::Usage;
 
 ### sub ###
