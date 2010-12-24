db4fbba8cdf86b1b0e672bab8cab8e933a73621b
tyru <tyru.exe@gmail.com>
2010-12-25 01:55:45 +0900

    vim - fix perl template

--- dotfiles/.vim/template/perl.pl
+++ dotfiles/.vim/template/perl.pl
@@ -1,13 +1,15 @@
 #!/usr/bin/env perl
-use strict;
-use warnings;
-use utf8;
+use common::sense;
+# use strict;
+# use warnings;
+# use utf8;
 
 # gnu_compat: --opt="..." is allowed.
 # no_bundling: single character option is not bundled.
 # no_ignore_case: no ignore case on long option.
 use Getopt::Long qw(:config gnu_compat no_bundling no_ignore_case);
 use Pod::Usage;
+
 
 sub usage () {
     pod2usage(-verbose => 2);
