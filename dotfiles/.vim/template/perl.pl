7af134db57500ea2334deaaf248ce1f71bfe6eaa
tyru <tyru.exe@gmail.com>
2012-10-03 01:12:10 +0900

    vim - perl template - add posix_default to Getopt::Long arguments

--- dotfiles/.vim/template/perl.pl
+++ dotfiles/.vim/template/perl.pl
@@ -7,7 +7,7 @@ use common::sense;
 # gnu_compat: --opt="..." is allowed.
 # no_bundling: single character option is not bundled.
 # no_ignore_case: no ignore case on long option.
-use Getopt::Long qw(:config gnu_compat no_bundling no_ignore_case);
+use Getopt::Long qw(:config posix_default gnu_compat no_bundling no_ignore_case);
 use Pod::Usage;
 
 
