b70a048244bd892eb917cf30e49035183c1c0431
tyru <tyru.exe@gmail.com>
2013-05-06 18:11:57 +0900

    fix make-symlink.sh for mingw environment

--- make-symlink.sh
+++ make-symlink.sh
@@ -1,7 +1,31 @@
 #/bin/sh
 
-cwd=`pwd`
-for i in `cat dotfiles.lst`; do
-	mkdir -p `dirname ~/$i`
-	ln "$@" -T -s $cwd/dotfiles/$i ~/$i && echo "created ~/$i"
+set -e
+
+cd `dirname $0`
+
+if [ "$MSYSTEM" = "MINGW32" ]; then
+	os_convert() {
+		re="^$1[ \t]+"
+		entry=`egrep "$re" dotfiles.lst.mingw || true`
+		if [ "$entry" ]; then
+			echo "$entry" | sed -re "s/$re//"
+		else
+			echo "$1"
+		fi
+	}
+else
+	os_convert() {
+		echo "$1"	# No conversion
+	}
+fi
+
+for from in `cat dotfiles.lst`; do
+	to=`os_convert "$from"`
+	mkdir -p `dirname "$HOME/$to"`
+	if ln -T -s "`pwd`/dotfiles/$from" "$HOME/$to"; then
+		echo "created $HOME/$to"
+	else
+		echo "error: Could not create $HOME/$to"
+	fi
 done
