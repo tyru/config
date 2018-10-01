817686993f8af926d6da62ed40a2471f088c65f4
tyru <tyru.exe@gmail.com>
2018-10-01 09:48:21 +0900

    don't clone submodule if already cloned

--- hooks/pre-update.d/01-clone-submodule.sh
+++ hooks/pre-update.d/01-clone-submodule.sh
@@ -3,6 +3,11 @@ set -ue
 # Skip if no repository exists
 [ "$repos" ] && [ -e "$pkgdir/$repos/.git" ] || exit 0
 
+cloned() {
+  git submodule status | awk "\$2 == '$1'" | grep -q '^[^-]'
+}
+
 # Clone submodule
-git submodule init $pkgdir/$repos
-git submodule update $pkgdir/$repos
+if ! cloned "$pkgdir/$repos"; then
+  git submodule update --init "$pkgdir/$repos"
+fi
